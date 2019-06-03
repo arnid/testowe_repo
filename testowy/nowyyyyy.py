8bed674e5ba9c6efef9e98a120abefca5ed5fba9
Arnold Dzionk <dzionkarni@gmail.com>
2019-06-04 00:07:08 +0200

    A few changes

--- testowy/nowyyyyy.py
+++ testowy/nowyyyyy.py
@@ -1,5 +1,20 @@
+#print("AHahhaha")
+import signal
+import time
+#import zmq
 
-
-
-
-print("AHahhaha")
+#context = zmq.Context()
+#socket = context.socket(zmq.REP)
+#socket.bind("tcp://*:5558")
+#
+# SIGINT will normally raise a KeyboardInterrupt, just like any other Python call
+try:
+    while(1):
+        print("duZZZZo")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("W: interrupt received, stopping…")
+#finally:
+#    # clean up
+#    socket.close()
+#    context.term()
